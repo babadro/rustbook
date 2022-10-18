6e37be11b2802067b130f6080aff6965e0a748d4
babadro <babadro@gmail.com>
2022-10-18 07:20:12 +0200

    4.3 The Slice Type completed

--- 04_understanding_ownership/slice_type/src/main.rs
+++ 04_understanding_ownership/slice_type/src/main.rs
@@ -20,9 +20,27 @@ fn main() {
 
     let slice = &s[0..len];
     let sclie = &s[..];
+
+    // String Slices as Parameters
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    let word = first_word(my_string_literal);
+
+    // Other Slices
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
