d2d1bf40b8a377a6730a820942f9371101eb77b2
babadro <babadro@gmail.com>
2022-10-21 06:23:40 +0200

    7.3 Paths for Referring to an Item in the Module Tree

---
+++ 07_managing_growing_projects_with_packages_crates_and_modules/restaurant2/src/lib.rs
@@ -0,0 +1,35 @@
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // will not compile. seasonal_fruit is a private field
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // Enums
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
