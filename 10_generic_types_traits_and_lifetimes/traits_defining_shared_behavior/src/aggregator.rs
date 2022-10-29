abf917c5364e4bc94132f5472fd95c79082eae71
babadro <babadro@gmail.com>
2022-10-29 08:29:52 +0200

    10.2 Traits: Defining Shared Behavior

---
+++ 10_generic_types_traits_and_lifetimes/traits_defining_shared_behavior/src/aggregator.rs
@@ -0,0 +1,78 @@
+// Defining a Trait
+pub trait Summary {
+    fn summarize(&self) -> String;
+
+    fn summarize_author(&self) -> String;
+
+    fn summarize_with_default(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+// Implementing a Trait on a Type
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// Using Trait Bounds to Conditionally Implement Methods
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+// Blanket implementations
+/*
+impl<T: Display> ToString for T {
+    // --snip--
+}
+
+let s = 3.to_string();
+ */
--- 10_generic_types_traits_and_lifetimes/traits_defining_shared_behavior/src/main.rs
+++ 10_generic_types_traits_and_lifetimes/traits_defining_shared_behavior/src/main.rs
@@ -1,33 +1,90 @@
+use crate::aggregator::NewsArticle;
+use aggregator::{Summary, Tweet};
+use std::fmt::Debug;
+use std::fmt::Display;
+
+mod aggregator;
+
 fn main() {
-    // Defining a Trait
-    pub trait Summary {
-        fn summarize(&self) -> String;
-    }
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-    // Implementing a Trait on a Type
-    pub struct NewsArticle {
-        pub headline: string,
-        pub location: String,
-        pub author: String,
-        pub content: String,
-    }
+    println!("1 new tweet: {}", tweet.summarize());
+    println!("Default impl: {}", tweet.summarize_with_default());
+}
 
-    impl Summary for NewsArticle {
-        fn summarize(&self) -> String {
-            format!("{}, by {} ({})", self.headline, self.author, self.location)
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Trait Bound Syntax
+pub fn notify2<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn notify3(item1: &impl Summary, item2: &impl Summary) {
+    // do something
+}
+
+pub fn notify4<T: Summary>(item1: &T, item2: &T) {
+    // do something
+}
+
+// Specifying Multiple Trait Bounds with the + Syntax
+pub fn notify5(item: &(impl Summary + Display)) {}
+
+pub fn notify6<T: Summary + Display>(item: &T) {}
+
+// Clearer Trait Bounds with where Clauses
+
+// verbose
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {
+    1
+}
+
+// the same, but shorter
+fn some_function2<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    1
+}
+
+// Returning Types that Implement Traits
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// wouldn't work
+/*
+fn returns_summarizable2(switch: bool) -> impl Summary {
+    if switch {
+        NewsArticle {
+            headline: String::from("Penguins win the Stanley Cup Championship!"),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
         }
-    }
-
-    pub struct Tweet {
-        pub username: String,
-        pub content: String,
-        pub reply: bool,
-        pub retweet: bool,
-    }
-
-    impl Summary for Tweet {
-        fn summarize(&self) -> String {
-            format!("{}: {}", self.username, self.content)
+    } else {
+        Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
         }
     }
 }
+*/
