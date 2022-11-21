bfffa099e90213301adf21c861d127a3520dce63
babadro <babadro@gmail.com>
2022-11-21 07:00:20 +0100

    15.4 Rc<T>, the Reference Counted Smart Pointer

---
+++ 15_smart_pointers/running_code_on_cleanup_with_the_drop_trait/src/main.rs
@@ -0,0 +1,29 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    // Running Code on Cleanup with the Drop Trait
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointers created.");
+
+    // Dropping a Value Early with std::mem::drop
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main");
+}
