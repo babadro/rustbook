3e33565c650f55a0c05cea56e5bf747740c23797
babadro <babadro@gmail.com>
2022-11-28 09:16:52 +0100

    18.1 All the Places Patterns Can Be Used

---
+++ 18_patterns_and_matching/all_the_places_patterns_can_be_used/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    // Conditional if let Expressions
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    // while let Conditional Loops
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    // for Loops
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    // let Statements
+    let x = 5;
+
+    let (x, y, z) = (1, 2, 3);
+
+    // let (x, y) = (1, 2, 3); expected a tuple with 3 elements, found one with 2 elements
+
+    // Function Parameters
+    fn print_coordinates(&(x, y): &(i32, i32)) {
+        println!("Current location: ({}, {})", x, y);
+    }
+
+    let point = (3, 5);
+    print_coordinates(&point);
+}
